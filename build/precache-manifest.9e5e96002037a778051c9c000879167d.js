9ff64b9e9428616359d4f34d612fe54eb470ac65
vtymoshchyk <vtymoshchyk@gmail.com>
2019-05-17 15:05:11 +0300

    added build folder for deploying on Github Pages

---
+++ build/precache-manifest.9e5e96002037a778051c9c000879167d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252c18c9ac33de6d8f6b4d5a0d4341cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "940cf96a319a4f613bf4",
+    "url": "/static/css/main.2ec714b4.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "940cf96a319a4f613bf4",
+    "url": "/static/js/main.a962dc58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
